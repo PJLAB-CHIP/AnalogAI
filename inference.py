5e5f8c35817ef69495194561194f62a33e6aba64
Eric-Russel <1036511081@qq.com>
2024-04-22 06:57:06 +0000

    change inference

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-created on 10.26
+created on 4.22
 version: 1.0
 train.py
 """
@@ -8,12 +8,13 @@ train.py
 
 # Imports
 import os
-os.environ['WANDB_API_KEY'] = 'e7a84490fccf4d551013cad7ca58549bb09594f7'
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
+# os.environ['WANDB_API_KEY'] = 'e7a84490fccf4d551013cad7ca58549bb09594f7'
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 from datetime import datetime
 from tqdm import tqdm
 # import timm
+import timm
 import numpy as np
 # Imports from PyTorch.
 import torch
@@ -22,56 +23,60 @@ from torch import max as torch_max
 import torch.nn.functional as F
 from torch.optim import lr_scheduler
 # Imports from networks.
-import timm
-from model import resnet_Q, resnet, vgg, vggQ, lenet, lenetQ, mobileNetv2, mobileNetv2Q, preact_resnet, vit
+
+from model import resnet, vgg, lenet, mobileNetv2, preact_resnet, vit
 # Imports from utils.
 from utils import create_optimizer, SAM, FGSMTrainer, PGDTrainer, train_step, test_evaluation
 from dataset import load_dataset
-# Imports from networks.
-from noise_inject import InjectForward, InjectWeight, InjectWeightNoise
-from earlystopping import EarlyStopping
 # from AnalogSram.sram_op import convert_to_sram_prepare
 from AnalogSram.convert_sram import convert_to_sram_prepare
 
 
 # from call_inference import infer_memtorch, infer_aihwkit, infer_MNSIM
-from call_inference import infer_aihwkit, infer_MNSIM
+# from call_inference import infer_aihwkit, infer_MemTorch
+from call_inference import infer_aihwkit
 import argparse
+import yaml
 import wandb
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value)
+    return namespace
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--project_name', default='AnalogAI', type=str, help='name')
 parser.add_argument('--architecture', default='vit', type=str, help='name')
 parser.add_argument('--dataset', default='cifar10', type=str, help='training dataset')
 parser.add_argument('--batch-size', default=512, type=int, help='mini-batch size')
-parser.add_argument('-lr',default=1e-1, type=float, help='learning rate')
-parser.add_argument('-momentum',default=9e-1, type=float, help='momentum')
-parser.add_argument('-weight_decay',default=0.0005, type=float, help='weight_decay')
-parser.add_argument('-epochs', default=200, type=int, help='sum of epochs')
-parser.add_argument('-n_classes', default=10, type=int, help='number of categories')
-parser.add_argument('--use_sgd', help='whether use sgd or adam', action='store_true')
-parser.add_argument('--injectforward', default=False, type=bool, help='whether inject forward noise or not')
-parser.add_argument('--mask', default=True, type=bool, help='whether inject forward with masked layers')
-parser.add_argument('--injectweightnoise', default=False, type=bool, help='whether inject weight noise or not')
-parser.add_argument('--injectweight', default=False, type=bool, help='whether inject noise or not')
-parser.add_argument('--mean', default=1., type=float, help='average value of noise')
-parser.add_argument('--sigma', default=1.0 / 6.0, type=float, help='variance of the noise')
-parser.add_argument('--SAM', default=False, type=bool, help='whether use SAM')
-parser.add_argument('-adaptive',default=False, type=bool, help='whether use ASAM')
+parser.add_argument('-sram_analog_recover',default=False, type=bool, help='whether to recover on an analog platform')
 parser.add_argument('-analog_infer',default=False, type=bool, help='whether to infer on an analog platform')
+
+parser.add_argument('--config', type=str, default='exp1_infer.yml', help='Path to the config file')
 args = parser.parse_args()
 
-config = vars(args)
-print(config)
+config_dir = './exp/'
+with open(config_dir + args.config, 'r') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+config = dict2namespace(config)
 
 # Device to use
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+device_cpu = torch.device("cpu")
+
 # Path to store datasets
-data_dir = os.path.join(os.getcwd(), "data", "DATASET")
+data_dir = os.path.join(os.getcwd(), "data", config.data.dataset)
 
 # Path to store results
-save_dir = os.path.join(os.getcwd(),'save_model', 'RESNET')
+save_dir = './save_model/' + config.data.architecture
+model_path = config.data.architecture + '.pth'
+save_path = os.path.join(save_dir, model_path)
 
 # Training parameters
 random_seed = 2024
@@ -132,43 +137,65 @@ def main():
     train_data, validation_data = dataset.load_images()
 
     #----Load the pytorch model------
-    # model = resnet_Q.ResNet18Qsram()
-    # model = resnet.ResNet18()
-    # model = timm.create_model('mobilenetv2_100', pretrained=False, num_classes=10)
-    # model = mobileNetv2.MobileNetV2()
-    model = vit.ViT()
-    # model = timm.create_model('resnet18', pretrained=False, num_classes=10)
-    model.to(device)
+
+    model = resnet.ResNet18()
+
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)     
-    
+    model.to(device)
 
     criterion = nn.CrossEntropyLoss().cuda()
 
     #----load existing model---------
-    # if os.path.exists(os.path.join(save_dir, 'checkpoint_139_91.880000.pth.tar')):
-    if os.path.exists('/code/AnalogAI/save_model/RESNET/vit_72_81.690000.pth.tar'):
+    if os.path.exists(os.path.join(save_dir, 'checkpoint_.pth.tar')):
         print('==> loading existing model')
-        model.load_state_dict(torch.load('/code/AnalogAI/save_model/RESNET/vit_72_81.690000.pth.tar'))    
+        model.load_state_dict(torch.load(os.path.join(save_dir, 'checkpoint_.pth.tar')))    
     #----------------------
-    wandb.login()
-    wandb.init(project="AnalogAI", config=config)
-    
-    ps = [16, 32, 64, 128]
-    es = np.linspace(0, 300, num=4, endpoint=True)
-    # infer_model = infer_memtorch().patch(model)
-    for p in ps:
-        for e in es: 
-            # infer_model = infer_aihwkit(forward_w_noise=n_w).patch(model)
-            infer_model = convert_to_sram_prepare(model=model, device=device, backend='SRAM', parallelism=int(p),
-            error=int(e),)
-            # print(infer_model)
-            _, _, error, accuracy = test_evaluation(
-                            validation_data, infer_model, criterion, device
-                        )
-            print(f'error:{error:.2f}' + f'accuracy:{accuracy:.2f}' + f' parallelism:{p:.4f}' + f' error:{e:.4f}')
-            wandb.log({'parallelis':p, 'error':e, 'accuracy':accuracy})
+    # wandb.login()
+    # wandb.init(project="AnalogAI", config=config)
 
+    # infer
+    if config.inferemce.platform.sram:
+        print("==> inferencing on SRAM") 
+        ps = [16, 32, 64, 128]
+        es = np.linspace(0, 300, num=4, endpoint=True)
+        for p in ps:
+            for e in es: 
+                infer_model_sram = convert_to_sram_prepare(model=model, 
+                                                      device=device,
+                                                      backend='SRAM', 
+                                                      parallelism=int(p),
+                                                      error=int(e),)
+                
+                _, _, error, accuracy = test_evaluation(
+                                validation_data, infer_model_sram, criterion, device
+                            )
+                print(f'error:{error:.2f}' + f'accuracy:{accuracy:.2f}' + f' parallelism:{p:.4f}' + f' error:{e:.4f}')
+                # wandb.log({'parallelis':p, 'error':e, 'accuracy':accuracy})
+
+    if config.inferemce.platform.aihwkit:
+        print("==> inferencing on IBM") 
+        w = np.linspace(0., 0.02, num=5, endpoint=True)
+        for n_w in w:
+            infer_model_aihwkit = infer_aihwkit(forward_w_noise=n_w).patch(model)
+            _, _, error, accuracy = test_evaluation(
+                            validation_data, infer_model_aihwkit, criterion, device_cpu
+                        )
+            print(f'error:{error:.2f}' + f'accuracy:{accuracy:.2f}' + f' w_noise:{n_w:.4f}')
+
+    if config.inferemce.platform.memtorch:
+        """
+        unfinished
+        """
+        print("==> inferencing on MemTorch") 
+        w = np.linspace(0., 0.02, num=5, endpoint=True)
+        for n_w in w:
+            # infer_model = infer_MemTorch(forward_w_noise=n_w).patch(model)
+            # _, _, error, accuracy = test_evaluation(
+            #                 validation_data, infer_model, criterion
+            #             )
+            print(f'error:{error:.2f}' + f'accuracy:{accuracy:.2f}' + f' w_noise:{n_w:.4f}')
+    
     # wandb.finish()
 
 if __name__ == "__main__":
