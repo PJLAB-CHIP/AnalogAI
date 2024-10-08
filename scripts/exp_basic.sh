1b8f131bef8daf650f05031f9838d221fee7edda
root <1036511081@qq.com>
2024-10-08 08:14:47 +0000

    update aicas repo

--- scripts/exp_basic.sh
+++ scripts/exp_basic.sh
@@ -1,12 +1,2 @@
-#! /bin/bash
+python train_basic.py --config base_example.yml
 
-# python train_fedprox.py --config resnet18_cifar10_asam.yml &
-# python train_fedprox.py --config resnet18_cifar10_FGSM.yml &
-# python train_fedprox.py --config resnet18_cifar10_sam.yml &
-# python train_fedprox.py --config resnet18_cifar10_PGD.yml &
-
-
-python inference.py --config resnet18_cifar10_asam.yml &
-python inference.py --config resnet18_cifar10_FGSM.yml &
-python inference.py --config resnet18_cifar10_sam.yml &
-python inference.py --config resnet18_cifar10_PGD.yml &
--- scripts/train_msnat.sh
+++ scripts/train_msnat.sh
@@ -1,4 +1,4 @@
-python train_fedprox.py \
+python train_msnat.py \
     --config resnet18_cifar10_client_5_epoch_1_noise_0.1_0.3_T_F.yml \
     --device cuda:0 \
     --comm_round 200 \
